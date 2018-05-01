8fcc0d9db7889ed0c80c0bdb3ff1005454554813
Dan Nguyen <dansonguyen@gmail.com>
2018-05-01 14:51:10 -0700

    mkay

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from flask import Flask
 from flask import render_template, request
 from os import environ
+import requests 
+from urllib.parse import quote
+
 
 MAPBOX_TOKEN = environ.get('MAPBOX_ACCESS_TOKEN')
-
 myapp = Flask(__name__)
 
-def parse_geocode_response(txt):
-    """
-    Return a dictionary
-    """
+
+API_URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/{p}.json?access_token={t}'
+
+def geocode(place, token=MAPBOX_TOKEN):
+    url = API_URL.format(p=quote(place), t=token)
+    resp = requests.get(url)
+    data = resp.json() 
+    features = data['features']
+    feat = features[0]
+    d = {}
+    d['name'] = feat['place_name']
+    d['longitude'] = feat['geometry']['coordinates'][0]
+    d['latitude'] = feat['geometry']['coordinates'][1]
+    return d
 
 
 @myapp.route("/")
